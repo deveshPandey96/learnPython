b5071c97ab9dfb47de61c080af6bcd8ad4b67724
Devesh Pandey <deveshpandey787@gmail.com>
2024-11-08 21:45:16 +0530

    practiced absolute and relative file paths

---
+++ main2.py
@@ -0,0 +1,6 @@
+# with open("/Users/devesh/Desktop/my_file.txt") as file:
+with open("../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# /Users/devesh/PycharmProjects/learnPython
